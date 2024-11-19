e785f6631d5c7df67f27dfd04ef0a4757942f2ac
alexndl31 <alex.nedelcut@gmail.com>
2024-11-19 20:54:18 +0000

    Improved the ux

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function displayPoem(response) {
-  console.log("Poesia generata");
+  console.log("Poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,11 +15,15 @@ function generatePoem(event) {
 
   let apiKey = "4t7d303d7c1ea2908b8dfea4coaff2b4";
   let prompt = `User instructions:Generate an Italian poem about ${instructionsInput.value}`;
-  let context =
-    "You are a romantic and a poem expert and you love to write short poems.Separate each line with </br>.Your mission is to generate a 4 line poem in basic HTML.Do not show the word html .Make sure you fallow the user instructions.Sign the poem with Your favorite Italian AI.";
+  let context = `You are a romantic and a poem expert and you love to write short poems.Separate each line with a </br>.Your mission is to generate a 4 line poem in basic HTML.Do not show the word html .Make sure you fallow the user instructions.Sign the poem with Your favorite Italian AI.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generazione di poesia...");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="thinking">
+      Generating an Italian poem about ${instructionsInput.value}</div>`;
+
+  console.log("Generating an Italian poem...");
 
   axios.get(apiURL).then(displayPoem);
 }

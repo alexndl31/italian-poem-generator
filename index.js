ac0d6c08bc27eb5e92b05b066a88d2ecbbce7f09
alexndl31 <alex.nedelcut@gmail.com>
2024-11-19 18:48:00 +0000

    Added basic form handling and added typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Lassu sulle montagne",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let formPoemElement = document.querySelector("#form-for-poem");
+formPoemElement.addEventListener("submit", generatePoem);

f8be21045c3cf1f19ad30e7660f76a315dbda793
alexndl31 <alex.nedelcut@gmail.com>
2024-11-19 21:00:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
 
   let apiKey = "4t7d303d7c1ea2908b8dfea4coaff2b4";
   let prompt = `User instructions:Generate an Italian poem about ${instructionsInput.value}`;
-  let context = `You are a romantic and a poem expert and you love to write short poems.Separate each line with a </br>.Your mission is to generate a 4 line poem in basic HTML.Do not show the word html .Make sure you fallow the user instructions.Sign the poem with Your favorite Italian AI.`;
+  let context = `You are a romantic and a poem expert and you love to write short poems.Separate each line with a </br>.Do NOT show </br>.Your mission is to generate a 4 line poem in basic HTML.Do not show the word html .Make sure you fallow the user instructions.Sign the poem with Your favorite Italian AI.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
